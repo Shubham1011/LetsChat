c8123f670ecf12f75f3c8243fc6781a30576db3d
Shubham Palkar <shubhamspalkar@gmail.com>
2021-01-09 00:33:08 +0530

    almostdone

--- src/App.js
+++ src/App.js
@@ -2,14 +2,51 @@
 import './App.css'
 import ChatBox from './ChatBox'
 import SideBar from './SideBar'
-
+import Pusher from 'pusher-js'
+import React, { useState, useEffect } from "react";
+import axios from 'axios';
 function App() {
+const[messages,setMessages]=useState([]);
+
+  useEffect(()=>{
+    console.log('api called');
+    axios.get("http://localhost:8080/get").then(res=>{
+        setMessages(res.data);
+       
+    }).catch(e=>{
+      console.log(e.message);
+    })
+  },[])
+
+  useEffect(() => {
+    console.log('subscriber created');
+    const pusher = new Pusher('3e888fccfeb395bd6cc6', {
+      cluster: 'ap2'
+    });
+  
+    const channel = pusher.subscribe('chat');
+    channel.bind('addMessage', function(data) {
+     // alert(JSON.stringify(data));
+      setMessages([...messages,data])
+    });
+
+    return ()=>{
+      channel.unbind_all();
+      channel.unsubscribe();
+      console.log('unsubscribed');
+    }
+    
+    
+  }, [messages])
+
+  console.log(messages);
+
   return (
     <div className="App">
       <h1>LetsChat</h1><h5>Thats all we do</h5>
       <div className="app_body">
       <SideBar></SideBar>
-          <ChatBox/>
+          <ChatBox  messages={messages}/>
       </div>
     </div>
   );
--- src/ChatBox.js
+++ src/ChatBox.js
@@ -1,9 +1,56 @@
+import { Avatar, IconButton } from '@material-ui/core'
+import { AttachFileOutlined, InsertEmoticon, Mic, MoreVert, Search, VoiceChat } from '@material-ui/icons'
 import React from 'react'
 import './chatbox.css'
-function ChatBox() {
+function ChatBox({messages}) {
+
+const sendMessage=()=>{
+    alert('sent')
+}
+
+
+
     return (
         <div class='wholechat'>
-            hello
+            <div className="chatHeader">
+                <Avatar className="icon"></Avatar>
+                <div><h1>Room Name</h1>
+                <p>Last Seen today</p>
+                </div>
+                <div className="chatheaderright">
+                   
+                   <IconButton>
+                       <Search></Search>
+                   </IconButton>
+                   <IconButton><AttachFileOutlined/></IconButton> 
+                   <IconButton><MoreVert></MoreVert></IconButton>
+
+                </div>
+            </div>
+            <div class="chatWindow">
+              {
+                messages.map(mess=>(
+                  <div className={"all "+(mess.received?'receive':'none')}> <div className="chatname">{mess.name } </div> 
+                  <div className="message">  
+                  {mess.message}
+                    <div className="timestamp"> {mess.timeStamp}</div>
+                    </div>
+                    </div> 
+                ))
+              }
+             
+         
+                  
+                  
+                   
+                
+           </div>
+            <div className="typer">
+                <IconButton><InsertEmoticon></InsertEmoticon></IconButton>
+                <input type="text" placeholder="Type a message" ></input>
+                <button  onClick={sendMessage}>Send </button>
+                <IconButton> <Mic></Mic></IconButton>
+            </div>
         </div>
     )
 }
